5fe5fa5d2a0704dfdf331f4d8728c01a718406a2
Yangyj <757033985@qq.com>
2021-05-10 14:49:54 +0800

    修改
    修改文件夹

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/music" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/MusicPlayer" : "/",
   configureWebpack: {
     resolve: {
       alias: {
